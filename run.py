1bd8d9aafc86b215bb8f74647ab61be330ed7284
Pedro Cristo <pcristo35@gmail.com>
2022-02-23 11:04:53 +0000

    Add code for get_word function

--- run.py
+++ run.py
@@ -1,2 +1,29 @@
 import random
 import gspread
+
+
+from hangman_words import *
+
+
+
+
+
+def get_word():
+    """
+    Get a random word from the word list in hangman_words.py
+    """
+    random_word = random.choice(words)
+    return random_word.upper()
+
+
+
+
+
+
+
+
+
+
+
+
+
