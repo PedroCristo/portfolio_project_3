ecf365a6e4649b95d179cb6e25c46ffa15dddbc1
Pedro Cristo <pcristo35@gmail.com>
2022-02-23 11:27:59 +0000

    Add to the game fuction a while not guessed statement

--- run.py
+++ run.py
@@ -31,7 +31,20 @@ def game(random_word):
     print(F"{Fore.YELLOW}\n\tLET'S PLAY THE HANGMAN GAME!\n")
     print(f"""{Fore.YELLOW}\t
     YOU HAVE TO GUESS A WORD WITH {len(random_word)} LETTERS""")
-    print(display_hangman(attempts))  
+    print(display_hangman(attempts))
+    print(full_word)
+    print("\n")  
+    while not guessed and attempts > 0:
+        print(f"{Fore.RED}\n\tWRONG LETTERS GUESSED:\n\t{guessed_wrong}\n")
+        display_score()
+        if attempts > 1:
+            print(f"{Fore.YELLOW}\n\tYOU HAVE {attempts} attempts")
+        else:
+            print(f"{Fore.RED}\n\tYOU HAVE {attempts} ATTEMPT LEFT\n")
+        guess = input(f"""{Fore.CYAN}\t\t
+        GUESS A LETTER OR A WORD PLEASE:\n\t>>> """).upper()
+
+
 
 
 
