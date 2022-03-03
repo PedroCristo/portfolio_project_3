e728dfc7faa69403ba1993fe4e5540142d6564f0
Pedro Cristo <pcristo35@gmail.com>
2022-03-03 14:57:02 +0000

    Write the comment in 3 lines

--- hangman_extras.py
+++ hangman_extras.py
@@ -5,7 +5,9 @@ import time
 def typewriter(string):
     """
    Typewriter for the main sentence at the beginning of the game
-   The Python code for the typewriter was taken from the following tutorial: [Kwasii](https://www.youtube.com/watch?v=A_1THfBpCH
+   
+   The Python code for the typewriter was taken from the following tutorial:
+   [Kwasii](https://www.youtube.com/watch?v=A_1THfBpCH
    """
     for i in string:
         sys.stdout.write(i)
