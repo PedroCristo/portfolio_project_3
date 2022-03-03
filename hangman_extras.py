7d6dbc479e4d30f704d0661cc9058eafa9c7d82c
Pedro Cristo <pcristo35@gmail.com>
2022-03-03 14:50:01 +0000

    Add credits for the typewriter code

--- hangman_extras.py
+++ hangman_extras.py
@@ -4,9 +4,11 @@ import time
 
 def typewriter(string):
     """
-    Typewriter for the main sentence in the beginning of the game
-    """
+   Typewriter for the main sentence at the beginning of the game
+   The Python code for the typewriter was taken from the following tutorial: [Kwasii](https://www.youtube.com/watch?v=A_1THfBpCH
+   """
     for i in string:
         sys.stdout.write(i)
         sys.stdout.flush()
         time.sleep(0.05)
+    
